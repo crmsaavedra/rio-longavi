7e65e9016e6c935eeffb7571f5b4254509a4c607
crmsaavedra <crismasaav@gmail.com>
2026-08-29 13:34:08 -0400

    Massive cleanup: remove 1.5GB of unused assets and scripts

--- fix_mision.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-with open('nosotros.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('Recursos/Misión.webp', 'Recursos/Mision.webp')
-
-with open('nosotros.html', 'w', encoding='utf-8') as f:
-    f.write(content)
--- make_gallery.py
+++
@@ -1,9 +0,0 @@
-﻿import os
-html = '<html><body>'
-path = r'C:\Users\crism\Desktop\Proyectos\riolongavi\Recursos\Imagenes varias'
-for f in os.listdir(path):
-    if f.endswith('.jpeg') or f.endswith('.jpg'):
-        html += f'<div style="display:inline-block; margin:10px;"><img src="Recursos/Imagenes varias/{f}" height="200"><br>{f}</div>\n'
-html += '</body></html>'
-with open('gallery.html', 'w', encoding='utf-8') as f:
-    f.write(html)
