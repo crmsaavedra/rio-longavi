13fe52e957fd426b4974b75d7b990481c9ad9a19
crmsaavedra <crismasaav@gmail.com>
2026-08-31 17:43:46 -0400

    Añadir nuevo cuadro de telemetría y actualizaciones recientes

---
+++ clean_audio.py
@@ -0,0 +1,16 @@
+﻿import re
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Replace from <audio id="entrevista-audio" up to the </div></div></div></section> block
+content = re.sub(
+    r'\s*<audio id="entrevista-audio"[\s\S]*?<div class="audio-controls">[\s\S]*?</div>\s*</div>\s*</div>\s*</section>',
+    '\n  </div>\n</section>',
+    content
+)
+
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print("Audio remnant removed.")
